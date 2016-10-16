f1d209fa4296749c159189fe26c6f177d25ce629
James Liu <jamesjhliu@gmail.com>
2016-10-15 21:33:03 -0700

    loading data is FUCKING BROKEN

--- ext/popup.js
+++ ext/popup.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-	document.getElementById('status').textContent = "Nice meme!";
+	document.getElementById('status').textContent = getWPMData();
 });
+
+
+function getWPMData() {
+	chrome.storage.local.get({'wpmData': []}, data => {
+		var wpmData = data.wpmData;
+		return wpmData;
+	})
+}
+
+function saveWPMData(wpm) {
+	chrome.storage.local.get({'wpmData': []}, data => {
+		var wpmData = data.wpmData;
+		wpmData.push({wpm, timestamp: Date.now()});
+		chrome.storage.local.set({'wpmData': wpmData }, () => {
+			chrome.storage.local.get('wpmData', res => {console.log(res)});
+		})
+	})
+}
