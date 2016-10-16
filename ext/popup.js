34cd142170a9513fa0c33c2bd7d3b152435a47bd
James Liu <jamesjhliu@gmail.com>
2016-10-15 22:58:52 -0700

    charts and stuff pretty much work

--- ext/popup.js
+++ ext/popup.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-	document.getElementById('status').textContent = getWPMData();
-});
+	document.getElementById('status').textContent = 'Nice meme!';
 
-
-function getWPMData() {
-	chrome.storage.local.get({'wpmData': []}, data => {
-		var wpmData = data.wpmData;
-		return wpmData;
+	document.getElementById('status').addEventListener('click', () => {
+		chrome.tabs.create({url: 'dataviz.html'});
 	})
-}
+});
 
 function saveWPMData(wpm) {
 	chrome.storage.local.get({'wpmData': []}, data => {
 		var wpmData = data.wpmData;
-		wpmData.push({wpm, timestamp: Date.now()});
+		var c = new Date();
+		var timestamp = c.getFullYear() + '-' + (c.getMonth() + 1) + '-' +
+						c.getDate() + ' ' + c.getHours() + ':' + c.getMinutes() + ':' +
+						c.getSeconds();
+		wpmData.push({wpm, timestamp});
 		chrome.storage.local.set({'wpmData': wpmData }, () => {
 			chrome.storage.local.get('wpmData', res => {console.log(res)});
 		})
