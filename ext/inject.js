cf6ce0d72d63c9cd2e82b14c4895e7790697161a
James Liu <jamesjhliu@gmail.com>
2016-10-16 07:29:13 -0700

    toggle works

--- ext/inject.js
+++ ext/inject.js
@@ -9,16 +9,28 @@ function colourize() {
 	timerStart = Date.now();
 }
 
+var state = false;
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	if (request.getState) {
+		sendResponse({state: state});
+	}
+});
+
+
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
-		console.log(request.colourize);
-		if (request.colourize === true) {
-		  colourize();
-		  console.log('COLOURIZING');
-		} else if (request.colourize === false) {
-		    var timer = Date.now() - timerStart;
-			var wpm = Math.round((numWords/(timer/1000))*60);
-			console.log(`WPM: ${wpm}`)
-			chrome.runtime.sendMessage({wpm: wpm});
+		if (request.colourize) {
+			if (!state) {
+			  colourize();
+			  console.log('COLOURIZING');
+			} else {
+			    var timer = Date.now() - timerStart;
+				var wpm = Math.round((numWords/(timer/1000))*60);
+				console.log(`WPM: ${wpm}`)
+				chrome.runtime.sendMessage({wpm: wpm});
+			}
+			state = !state;
+			sendResponse({state:state});
 		}
 });
--- ext/popup.js
+++ ext/popup.js
@@ -1,9 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
+	var state = false;
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	  chrome.tabs.sendMessage(tabs[0].id, {getState: true}, function(response) {
+	    state = response.state;
+
+	    document.getElementById('enableTagging').checked = state;
+	  });
+	});
+
+
 	document.getElementById('enableTagging').addEventListener('click', () => {
-		var state = document.getElementById('enableTagging').checked;
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		  chrome.tabs.sendMessage(tabs[0].id, {colourize: state}, function(response) {
-		    //console.log(response.farewell);
+		  chrome.tabs.sendMessage(tabs[0].id, {colourize: true}, function(response) {
+    	    document.getElementById('enableTagging').checked = response.state;
 		  });
 		});
 	});
